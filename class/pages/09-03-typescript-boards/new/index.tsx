4e5e283b37d4fab82af34199654ccd50eb845552
hhjk00 <eunyu0604@gmail.com>
2022-03-24 14:25:06 +0900

    typescript

---
+++ class/pages/09-03-typescript-boards/new/index.tsx
@@ -0,0 +1,10 @@
+//등록하기 페이지
+
+import BoardWrite from "../../../src/components/commons/units/board/09-board-write/BoardWrite.contatiner";
+
+export default function BoardsNewPage() {
+
+    return (
+        <BoardWrite isEdit={false}/>
+    )
+}
